d2110e1ac7b8d69d7f033be334668faa681413a5
User <test@test.com>
2019-05-27 13:26:30 +0300

    mongodb connect

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 'use strict';
 
 const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
 
 // Constants
 const PORT = 3000;
-const HOST = '0.0.0.0'; 
+const HOST = '0.0.0.0';
+
+MongoClient.connect('mongodb://localhost:27017/animals', function(err, db) {
+  if (err) {
+    throw err;
+  }
+  db.collection('mammals').find().toArray(function(err, result) {
+    if (err) {
+      throw err;
+    }
+    console.log(result);
+  });
+});
 
 // App
 const app = express();
