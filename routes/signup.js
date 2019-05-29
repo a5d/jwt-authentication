cf621596993d25dd0b17022ab4ad597e0cced049
evgeniy <5108591@mail.ru>
2019-05-29 18:00:30 +0300

    signup

--- routes/signup.js
+++ routes/signup.js
@@ -1,25 +1,30 @@
-import {Router} from 'express';
+const Router = require('express').Router
 
 const router = Router();
 
 router.use((req, res, next) => {
   const user = req.body
-
-  req.db.find({email: user.email}).limit(1).toArray((users, err) => {
-    if (users.length > 0) {
-      res.status(404).json({error: 'Email already use'})
-    } else {
-      req.db.insertOne(user, (err) => {
-        if (err) {
-          res.status(404).json({error: 'Insert Error'})
-        } else {
-          res.json({msg: 'Registered'})
-        }
-      })
-    }
-  })
+console.log(user)
+  try {
+	  req.db.find({email: user.email}).limit(1).toArray((users, err) => {
+		  
+		if (users && users.length > 0) {
+		  res.status(404).json({error: 'Email already use'})
+		} else {
+		  req.db.insertOne(user, (err) => {
+			if (err) {
+			  res.status(404).json({error: 'Insert Error'})
+			} else {
+			  res.json({msg: 'Registered'})
+			}
+		  })
+		}
+	  })
+	} catch (err) {
+		res.status(404).json({error: err.message})
+	}
 
   next()
 });
 
-export default router
+module.exports = router
