29eb6031f839c285bf95a51957262f8ab30a2e1c
User <test@test.com>
2019-05-30 14:16:35 +0300

    signup

--- routes/api/signup.js
+++ routes/api/signup.js
@@ -9,24 +9,18 @@ router.use((req, res) => {
     req.db.find({email: user.email}).limit(1).toArray((err1, users) => {
       if (err1) {
         res.status(400).json({error: err1})
-        return
-      }
-
-      if (users.length > 0) {
+      } else if (users.length > 0) {
         res.status(400).json({error: 'Email already use'})
-        return
+      } else {
+        req.db.insertOne(user, (err2) => {
+          if (err2) {
+            res.status(400).json({error: 'Insert Error'})
+          } else {
+            res.json({msg: 'Registered'})
+          }
+        })
       }
-
-      req.db.insertOne(user, (err2) => {
-        if (err2) {
-          res.status(400).json({error: 'Insert Error'})
-          return
-        }
-
-        res.json({msg: 'Registered'})
-      })
     })
-
   } catch (err) {
     res.status(400).json({error: err.message})
   }
