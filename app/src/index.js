57b056014edd2ede83df7cfd664d2dc15ae0c7a2
evgeniy <5108591@mail.ru>
2019-06-05 23:07:43 +0300

    app init

---
+++ app/src/index.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react'
+import ReactDOM from 'react-dom'
+
+class FormContainer extends Component {
+  render() {
+    return <div>App</div>
+  }
+}
+
+ReactDOM.render(<FormContainer/>, document.getElementById('app'))
