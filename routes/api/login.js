bf1c79cbe0de1a08a70ce7c44c1e1d179d796a8b
evgeniy <5108591@mail.ru>
2019-06-18 22:09:45 +0300

    bcrypt

--- routes/api/login.js
+++ routes/api/login.js
@@ -1,17 +1,22 @@
+const bcrypt = require('bcrypt-nodejs');
 const jwt = require('jsonwebtoken')
 const {privateKey} = require('../../config')
 
 const router = (req, res) => {
-  req.db.find(req.body).limit(1).toArray((err, users) => {
-    if (users.length > 0) {
-      const token = jwt.sign(users[0], privateKey)
-      res
-        .cookie('jwt', token, {expires: new Date(Date.now() + 12900000), path: '/'})
-        .json({msg: 'Logged'})
-      return
-    }
+  const {email, password} = req.body
 
-    res.status(400).json({error: 'Not found 1'})
+  bcrypt.hash(password, 10, null, (err, hash) => {
+    req.db.find({email, password: hash}).limit(1).toArray((err, users) => {
+      if (users.length > 0) {
+        const token = jwt.sign(users[0], privateKey)
+        res
+          .cookie('jwt', token, {expires: new Date(Date.now() + 12900000), path: '/'})
+          .json({msg: 'Logged'})
+        return
+      }
+
+      res.status(400).json({error: 'Not found 1'})
+    })
   })
 }
 
