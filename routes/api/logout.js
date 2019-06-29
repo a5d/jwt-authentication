f75b2aac2631df5bd5232a5df4ac585b7485ccb9
evgeniy <5108591@mail.ru>
2019-06-29 11:09:48 +0300

    delete backend

--- routes/api/logout.js
+++
@@ -1,6 +0,0 @@
-const router = (req, res) => {
-  res.cookie('jwt', -1, {expires: new Date(Date.now() - 900000), path: '/'})
-  res.json({msg: 'Logout'})
-}
-
-module.exports = router
--- routes/not-found.js
+++
@@ -1,9 +0,0 @@
-const {Router} = require('express')
-
-const router = Router()
-
-router.use((req, res) => {
-  res.status(404).send('not found')
-})
-
-module.exports = router
